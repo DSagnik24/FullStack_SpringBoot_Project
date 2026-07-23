4b46313649a20e147755a4a2fff4a0df00b3e527
DSagnik24 <sagnikduttaofficial18@gmail.com>
2026-07-23 09:07:29 +0530

    Update CategoryController

--- src/main/java/com/sagnik/Ecom/controller/CategoryController.java
+++ src/main/java/com/sagnik/Ecom/controller/CategoryController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class CategoryController {
+public class  CategoryController {
     @Autowired
     private CategoryService categoryService;
 
